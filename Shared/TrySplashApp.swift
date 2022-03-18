1b982e4b76033686b8c0f9b3ff6335bcdf1f671e
Dan Hart <dhart94@gmail.com>
2022-03-18 14:23:20 -0400

    Initial Commit

---
+++ Shared/TrySplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrySplashApp.swift
+//  Shared
+//
+//  Created by Dan Hart on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct TrySplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
