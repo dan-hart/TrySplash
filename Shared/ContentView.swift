06d9a55c354fced18afc8feb3c47c697a5871d75
Dan Hart <dhart94@gmail.com>
2022-03-18 14:35:18 -0400

    Implement Splash

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -6,11 +6,36 @@
 //
 
 import SwiftUI
+import Splash
 
 struct ContentView: View {
+    var font: Splash.Font {
+        .init(size: 18)
+    }
+    
+    var theme: Splash.Theme {
+        .presentation(withFont: font)
+    }
+    
+    var highlighter: Splash.SyntaxHighlighter<AttributedStringOutputFormat> {
+        SyntaxHighlighter(format: AttributedStringOutputFormat(theme: theme))
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NSASLabel { label in
+            label.attributedText = highlighter.highlight("func helloWorld() -> String")
+        }
+        .padding()
+    }
+}
+
+struct NSASLabel: UIViewRepresentable {
+    typealias TheUIView = UILabel
+    fileprivate var configuration = { (view: TheUIView) in }
+
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> TheUIView { TheUIView() }
+    func updateUIView(_ uiView: TheUIView, context: UIViewRepresentableContext<Self>) {
+        configuration(uiView)
     }
 }
 
